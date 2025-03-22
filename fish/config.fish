4556e24d24f1156768e32d55f350b4f858026910
wen0523 <1545255656@qq.com>
2025-03-22 11:51:08 +0800

    config

---
+++ fish/config.fish
@@ -0,0 +1,42 @@
+set -g fish_greeting
+
+if status is-interactive
+    starship init fish | source
+end
+
+# List Directory
+alias ls="lsd"
+alias l="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias lt="ls --tree"
+
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
+
+set -x PATH /opt/intellij-idea-ultimate-edition/bin $PATH
+set -x JAVA_HOME /usr/lib/jvm/default
+
+
+thefuck --alias | source
+
+# Add pyenv executable to PATH by running
+# the following interactively:
+
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+
+# Load pyenv automatically by appending
+# the following to ~/.config/fish/config.fish:
+
+pyenv init - fish | source
+
+
+# Restart your shell for the changes to take effect.
